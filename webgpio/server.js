9b2d12552d05ea1738ce49330211abbcb380b570
Gang Zhang <gzhangx@hotmail.com>
2018-06-21 00:56:30 -0400

    link

--- webgpio/server.js
+++ webgpio/server.js
@@ -3,7 +3,10 @@ const restify = require('restify');
 const server = restify.createServer();
 server.use(restify.plugins.queryParser());
 server.get('/', (req, res, next)=>{
-  res.send('OK V1.0');
+  res.header('content-type','text/html');
+  res.contentType = 'text/html';
+  res.sendRaw('OK V1.0 <a href="/onoff/left/1">left on</a>');
+  //res.sendRaw('test');
 });
 
 server.get('/onoff/:who/:onoff', (req,res,next)=>{
