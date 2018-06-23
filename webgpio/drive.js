f4437e651586f2699dbba5a5ce105ed70f6662ae
gang zhang <gang.zhang@pps.io>
2018-06-23 16:09:10 -0400

    drive

---
+++ webgpio/drive.js
@@ -0,0 +1,45 @@
+const gon = require('./ngpio');
+
+const gleft = gon.createOutGpio(5);
+const gright = gon.createOutGpio(6);
+//g5.gon();
+
+//setTimeout(()=>{
+//  g5.goff();
+//  g5.end();
+//}, 5000);
+
+const states = {
+  forward: 'forward',
+    left:'left',
+    right:'right',
+    stop: 'stop'
+};
+
+let curState = states.stop;
+function drive() {
+  setTimeout(drive, 1000);
+  switch(curState) {
+      case states.stop:
+          gleft.goff();
+          gright.goff();
+          break;
+      case states.left:
+          gleft.gon();
+          gright.goff();
+          break;
+      case states.right:
+          gright.gon();
+          gleft.goff();
+          break;
+      case states.forward:
+          gright.gon();
+          gleft.gon();
+          break;
+  }
+}
+drive();
+module.exports = {
+    states,
+    setState: s => curState = s
+};
