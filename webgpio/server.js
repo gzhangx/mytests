5d45e8b3c608a17c188f3e4e383079b45ad4b9cb
gang zhang <gang.zhang@pps.io>
2018-06-23 16:08:45 -0400

    latest

--- webgpio/server.js
+++ webgpio/server.js
@@ -1,15 +1,19 @@
 const restify = require('restify');
 
 const server = restify.createServer();
+const {states, setState} = require('./drive');
 server.use(restify.plugins.queryParser());
 server.get('/', (req, res, next)=>{
   res.header('content-type','text/html');
   res.contentType = 'text/html';
-  res.sendRaw('OK V1.0 <a href="/onoff/left/1">left on</a>');
+  res.sendRaw('OK V1.0 <a href="/onoff/forwrd">foward</a><br>' +
+      '<a href="/onoff/left">left</a><br>' +
+      '<a href="/onoff/right">right</a><br>' +
+      '<a href="/onoff/stop">stop</a><br>');
   //res.sendRaw('test');
 });
 
-server.get('/onoff/:who/:onoff', (req,res,next)=>{
+server.get('/onoff/:what', (req,res,next)=>{
   res.send(req.params);
 });
 server.listen(8080, ()=>{});
