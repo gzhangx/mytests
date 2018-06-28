369a43ffe8e3e903ac75ceb27b7fc5008f8f8c65
Gang Zhang <gzhangx@hotmail.com>
2018-06-28 10:35:10 -0400

    fixing gpio state

--- webgpio/ngpio.js
+++ webgpio/ngpio.js
@@ -3,10 +3,14 @@ const Gpio = require('onoff').Gpio;
 
 function createOutGpio(who){
   const created = new Gpio(who, 'out');
+  function onoff(v){
+    created.writeSync(v);
+  }
+  onoff(1);
   return {
     gpio: created,
-    gon: ()=>created.writeSync(1),
-    goff: ()=>created.writeSync(0),
+    gon: ()=>onoff(0),
+    goff: ()=>onoff(1),
     end: ()=>created.unexport()
   }
 }
