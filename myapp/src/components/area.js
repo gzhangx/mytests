13ed9e30ab22285d3e63ac5df8ea985a6d15dfcf
gang zhang <gang.zhang@pps.io>
2018-06-30 10:02:05 -0400

    adding drive

--- myapp/src/components/area.js
+++ myapp/src/components/area.js
@@ -5,13 +5,15 @@ import './area.css';
 import gpio from '../api/gpio';
 
 function YourComponentOne(props) {
+    const steeringValCenter = 100;
     const centerX = 200;
     const centerY = 200;
     const x = get(props,'position.x')- centerX;
     const y = get(props,'position.y')- centerY;
     const lr = x > 50? 'right' : x<-50?'left':'center';
     const fs = y > 50? 'foward' :'stop';
-    gpio.steer(x+ centerX);
+    gpio.steer(x+ steeringValCenter);
+    gpio.drive(fs);
   return <div class="flex-container">
           <span margin="5">{get(props,'position.x')}</span>
           <span margin="5">{get(props,'position.y')}</span>
